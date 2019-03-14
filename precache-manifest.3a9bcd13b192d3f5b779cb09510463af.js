0332c8725c96f91c479b54f0a97fa1ec6d8579de
umas sunavan <fareastsunflower@gmail.com>
2019-03-14 16:29:13 +0800

    Updates

---
+++ precache-manifest.3a9bcd13b192d3f5b779cb09510463af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eff07cd92490161accd2",
+    "url": "/paging-paging/static/js/runtime~main.eff07cd9.js"
+  },
+  {
+    "revision": "1f87c296f60ca9ab497e",
+    "url": "/paging-paging/static/js/main.1f87c296.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/paging-paging/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "1f87c296f60ca9ab497e",
+    "url": "/paging-paging/static/css/main.c48a2679.chunk.css"
+  },
+  {
+    "revision": "9303d11e0d204f7f183aeae683c483a5",
+    "url": "/paging-paging/index.html"
+  }
+];
