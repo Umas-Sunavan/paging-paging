1185ad9ed65d92db6992f8651825cf9cf53b79b6
umas sunavan <fareastsunflower@gmail.com>
2019-09-24 22:05:46 +0800

    update link and intro text

---
+++ precache-manifest.6689cdb6cfb8215041783dfd12269663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0232dbab0ac883cc287e",
+    "url": "/static/js/main.0232dbab.chunk.js"
+  },
+  {
+    "revision": "7109618d32366e3e1d32",
+    "url": "/static/js/1.7109618d.chunk.js"
+  },
+  {
+    "revision": "0232dbab0ac883cc287e",
+    "url": "/static/css/main.1d156b1d.chunk.css"
+  },
+  {
+    "revision": "67c3819a1dffc807d60ecff4c443dae6",
+    "url": "/index.html"
+  }
+];
